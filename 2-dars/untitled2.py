b4873b18993062c4c1bc1c1870d1482d640aa67f
davron120 <108650250+davron120@users.noreply.github.com>
2022-07-05 16:42:27 +0500

    Add files via upload

---
+++ 2-dars/untitled2.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 20:53:56 2022
+
+@author: User
+"""
+
+print('Men "acer" rusumli noutbuk sotib oldim')
+print('''Men dasturlashni endi, 
+      o'rganishni boshladim''')
+print("Mohirdevga omad \ntilagan holda Davronbek Yo'ldoshev")
+print(2**5)
+print(5/3)
+print(12//5)
+print(52%8)
+print(' "Tiko", "Damas", "Nexia" ')
+
+#5 ning 4-darajasini toping
+print(5**4)
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print(22%4)
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("kvadratning yuza=", 125^2," peremetri esa=", 4*125)
+
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping  (pi=3.14 deb oling)
+d=21
+s=3.14*d*d/4
+print('doira yuzi=', s)
+
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping
+a=6
+b=7
+c=(a*a+b*b)**(1/2)
+print('gepatinuza=', c)
