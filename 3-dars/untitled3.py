e9b67ad9093f7e3e28a8441e078a437b85d630a6
davron120 <108650250+davron120@users.noreply.github.com>
2022-07-05 16:42:52 +0500

    Add files via upload

---
+++ 3-dars/untitled3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 21:11:34 2022
+
+@author: User
+"""
+
+ism="Davronbek"
+yosh=19
+print(ism)
+print(yosh)
+
+ism='Davronbek'
+print(ism)
+ism="DAvronbek"
+print(ism)
+
+#"Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring.
+matn="hello world"
+print(matn)     
+
+#xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring,
+# keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar="Men dasturiy injiniring yo'nalishida o'qiyman"
+print(xabar)
+xabar="fakultetim raqamli texnalogiya"
+print(xabar)
+
+#Aylananing radiusi va yuzini toping
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
