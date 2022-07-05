34793053b69ae5f1af0b8a6e0c1a4c4be8118f9e
davron120 <108650250+davron120@users.noreply.github.com>
2022-07-05 16:43:36 +0500

    Add files via upload

---
+++ 5-dars/untitled5.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 25 08:33:51 2022
+
+@author: User
+"""
+
+#SONLAR MAVZUSI
+#DASTURCHI DAAVRONBEK
+
+'''a=6
+b=-7
+c=a-b
+print(c)
+
+#kvadratning yuzini toping
+kvadrat_tomoni=6
+yuzi=kvadrat_tomoni**2
+print(yuzi)
+
+#doira yuzi
+pi=3.14
+d=6
+yuza=pi*d*d/4
+print(yuza)
+
+#bo'linmani toping
+a=6
+b=a/2
+print(b)
+
+#hisoblang
+a=15.0
+b=a+6
+c=a*3
+d=a/5
+e=a//5
+print(b ,' ', c,' ', d,' ', e)
+aholi_soni=7_567_900_000
+print("yer yuzasida", aholi_soni,"ga yaqin odam yashaydi ")
+
+#bir nechta o'zgaruvchiga birdan qiymat berish
+a, n, c, k=5,6,9,12
+print(a, n, c, k)
+
+#string sinfining qo'llanilishi
+ism='davronbek'
+yosh=19
+print(ism+' '+str(yosh),'yoshda')
+
+#o'zgaruvchining tipini tekshirish 'type'
+
+ism='Davronbek'
+yosh=19
+print(type(ism))
+print(type(yosh))
+
+#tugilgan yilingni topaman
+ism=input('isming nima?\n>>> ')
+yosh=int(input('yoshing nechida?\n>>>'))
+print(ism, "ning tug'ilgan yili: ", 2022-yosh)'''
+
+#Foydalanuvchi kiritgan sonning kvadrati va kubini konsolga chiqaruvchi dastur
+#tuzing
+son=int(input('\nson kiriting:\n>>> '))
+kv=son**2
+kub=son**3
+print('sonning kvadrati: ', kv,'kubi esa: ', kub)
+
+son1=int(input('1-sonni kiriting: '))
+son2=int(input('2-sonni kiriting: '))
+print(f"{son1}+{son2}=",son1+son2)
+print(f"{son1}-{son2}=",son1-son2)
+print(f"{son1}*{son2}=",son1*son2)
+print(f"{son1}/{son2}=",son1/son2)
