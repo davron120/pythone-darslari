74fd587edf0f1986294a1d235913a8d72d889fed
davron120 <108650250+davron120@users.noreply.github.com>
2022-07-05 16:43:13 +0500

    Add files via upload

---
+++ 4-dars/temp4.2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#4-dars.davomi
+
+'''meva="   olma   "
+print("Men ",meva.strip(),"mevasini yaxshi ko'raman")
+print("men ",meva.rstrip(),"mevasini yomon ko'raman")
+print("Men bugun ",meva.lstrip(),"mevasini sotib olaman")
+print(meva)
+
+meva="  sabzi   "
+print(meva.lstrip(),"meniki")
+print(meva.rstrip(),"meniki")
+print(meva.strip(),"meniki")'''
+
+#ism=input("ismingizni kiriting: ")
+#print("Assalomu alaykum ", ism.title())
+
+kocha=input("ko'changizni kiriting:\n>>> ")
+mahalla=input("mahallangizni kiriting:\n>>> ")
+tuman=input("tumaningizni kiriting:\n>>> ")
+viloyat=input("viloyatingizni kiriting:\n>>>")
+print(f"men \n {viloyat} viloyati,\n  {tuman} tumani,\n {mahalla} mahallasi,\n {kocha} ko'chasida yashayman")
---
+++ 4-dars/untitled4.1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 21:21:24 2022
+
+@author: User
+"""
+
+ism='Davronbek'
+print('mening ismim '+ism)
+
+ism = 'Davronbek'
+familya="Yo'ldoshev"
+print(ism+' '+familya)
+
+ism="Navro'zbek"
+familya="Yo'ldoshev"
+ism_familya=f"{ism} {familya}"
+print(ism_familya)
+
+ism ="Asadbek"
+familya= "Istatov"
+print(f"Salom mening ismim {ism}.{ism} {familya}")
+
+ism="Davronbek"
+filamya="Yo'ldOSHEV"
+sharif=(f"ismim {ism}, familyaam {filamya}")
+print(sharif.upper())
+
+ism="Davronbek"
+filamya="Yo'ldOSHEV"
+sharif=(f"ismim {ism}, familyaam {filamya}")
+print(sharif.capitalize())
+print(sharif.title())
+print(sharif.lower())
