9a47568f3f3fc66c82f86a93f391b8fe30570042
davron120 <108650250+davron120@users.noreply.github.com>
2022-07-05 16:42:04 +0500

    Add files via upload

---
+++ 1-dars/untitled1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 20:48:01 2022
+
+@author: User
+"""
+#24//06//2022. Juma.
+#//Yo'ldoshev Davronbek
+print("2+4=", 2+4)
+print("2*5+4=",2*5+4)
+print("Salom hammaga")
+print("Mening ismim Davronbek, familyam Yo'ldoshev")
